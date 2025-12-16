a891357819e159014df5c009936b030715368664
lza19 <maxx5611056110@gmail.com>
2025-12-16 21:27:14 +0700

    12

---
+++ test/1.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+if a > 0:
+    print("เต็มบวก")
+    if a % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+else :
+    print("ไม่ใช่เต็มบวก")
---
+++ test/2.py
@@ -0,0 +1,7 @@
+x = int(input())
+
+
+if x > 0:
+    print("more than zero")
+else:
+    print("less or equal to zero")
